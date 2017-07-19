7014a9dbc57aa4fb03b3f50308ef6354d05dc791
Sergii Naumov <sergii.naumov@siemens.com>
2017-07-19 15:37:42 +0200

    SubWordNavigation

--- src/main.js
+++ src/main.js
@@ -5,10 +5,18 @@ const onCommandToggleAnchor = require('./lib/anchor-commands').onCommandToggleAn
 const onCommandActivateCursors = require('./lib/anchor-commands').onCommandActivateCursors;
 const onCommandCleanAnchors = require('./lib/anchor-commands').onCommandCleanAnchors;
 const onDidChangeTextDocument = require('./lib/anchor-commands').onDidChangeTextDocument;
+const camelCaseNav = require('./lib/camel-case-nav');
+
 
 module.exports = {
   activate (context) {
+    const navigator = new camelCaseNav.CamelCaseNav();
+
     context.subscriptions.push(
+      vscode.commands.registerTextEditorCommand('cursorWordStartLeft', editor => navigator.doAction(editor, camelCaseNav.DIRECTION_LEFT, camelCaseNav.ACTION_MOVE)),
+      vscode.commands.registerTextEditorCommand('cursorWordEndRight', editor => navigator.doAction(editor, camelCaseNav.DIRECTION_RIGHT, camelCaseNav.ACTION_MOVE)),
+      vscode.commands.registerTextEditorCommand('cursorWordStartLeftSelect', editor => navigator.doAction(editor, camelCaseNav.DIRECTION_LEFT, camelCaseNav.ACTION_SELECT)),
+      vscode.commands.registerTextEditorCommand('cursorWordEndRightSelect', editor => navigator.doAction(editor, camelCaseNav.DIRECTION_RIGHT, camelCaseNav.ACTION_SELECT)),
       vscode.commands.registerTextEditorCommand('extension.cursor-tools.anchor', onCommandToggleAnchor),
       vscode.commands.registerTextEditorCommand('extension.cursor-tools.activate', onCommandActivateCursors),
       vscode.commands.registerTextEditorCommand('extension.cursor-tools.clean', onCommandCleanAnchors)
--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,38 @@ module.exports = {
   target: 'node',
   output: {
     libraryTarget: 'commonjs2',
-    filename: 'dist/extension.js'
+    filename: 'dist/extension.js',
+    devtoolModuleFilenameTemplate: '[absolute-resource-path]'
   },
   externals: {
     'vscode': 'vscode'
   },
+  devtool: 'source-map',
   module: {
-    loaders: [{
-      test: /json$/,
-      loader: 'json'
-    }]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                "env",
+                {
+                  targets: {
+                    node: 6
+                  },
+                  modules: false,
+                  loose: true,
+                  useBuiltIns: true,
+                  debug: true
+                }
+              ]
+            ]
+          }
+        }
+      }
+    ]
   }
 };
